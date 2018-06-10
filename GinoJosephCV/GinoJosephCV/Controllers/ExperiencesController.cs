52e90bf1f03a283878596b5538d60d0c8452af2b
Gino Joseph <gino@limabean.co.za>
2018-06-10 15:05:13 +0200

    Build ExperienceModel ExperienceRoles and ExperiencesController

---
+++ GinoJosephCV/GinoJosephCV/Controllers/ExperiencesController.cs
@@ -0,0 +1,29 @@
+﻿using GinoJosephCV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GinoJosephCV.Controllers
+{
+    public class ExperiencesController : Controller
+    {
+        // GET: Experiences
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Experiences
+        public ActionResult ListExperiences()
+        {
+            List<ExperienceModel> experiences = new List<ExperienceModel>();
+            experiences.Add(new ExperienceModel { employer = "Employer1", jobTitle = "Job Ttle1", summary = "Summary1", roles = { new ExperienceRole { roleDescription = "Description 1" }, new ExperienceRole { roleDescription = "Description 1" } } });
+            experiences.Add(new ExperienceModel { employer = "Employer2", jobTitle = "Job Ttle2", summary = "Summary2", roles = { new ExperienceRole { roleDescription = "Description 1" }, new ExperienceRole { roleDescription = "Description 2" } } });
+
+
+            return View();
+        }
+    }
+}
---
+++ GinoJosephCV/GinoJosephCV/Models/ExperienceModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GinoJosephCV.Models
+{
+    public class ExperienceModel
+    {
+        public string employer { get; set; } = "";
+        public string jobTitle { get; set; } = "";
+        public string summary { get; set; } = "";
+        public List<ExperienceRole> roles;
+
+
+    }
+
+    public class ExperienceRole
+    {
+        public string roleDescription { get; set; }
+    }
+}
